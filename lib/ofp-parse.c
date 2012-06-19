a709887bd409c0d14ddeec1a991794ad9f580091
Isaku Yamahata <yamahata@valinux.co.jp>
2012-06-19 16:39:50 +0900

    OF 1.1 VLAN QinQ changes. From: Ravi K <rkerur@gmail.com>
    Signed-off-by: Ravi K <rkerur@gmail.com>

--- lib/ofp-parse.c
+++ lib/ofp-parse.c
@@ -288,6 +288,10 @@ parse_named_action(enum ofputil_action_code code, const struct flow *flow,
     uint8_t pcp;
     uint8_t tos;
 
+    /* XXX: TODO VVVVVVVVVVVVVVVVV */
+    struct nx_action_push_vlan *navpush;
+    /* XXX: TODO ^^^^^^^^^^^^^^^^^ */
+
     switch (code) {
     case OFPUTIL_ACTION_INVALID:
         NOT_REACHED();
@@ -456,6 +460,13 @@ parse_named_action(enum ofputil_action_code code, const struct flow *flow,
     case OFPUTIL_NXAST_SET_MPLS_TC:
         ofpact_put_SET_MPLS_TC(ofpacts)->mpls_tc = str_to_u32(arg);
         break;
+
+    /* XXX: TODO VVVVVVVVVVVVVVVVV */
+    case OFPUTIL_NXAST_PUSH_VLAN:
+        navpush = ofputil_put_NXAST_PUSH_VLAN(b);
+        navpush->tpid = htons(str_to_u32(arg));
+        break;
+    /* XXX: TODO ^^^^^^^^^^^^^^^^^ */
 
     case OFPUTIL_NXAST_SET_MPLS_TTL:
         ofpact_put_SET_MPLS_TTL(ofpacts)->mpls_ttl = str_to_u32(arg);
