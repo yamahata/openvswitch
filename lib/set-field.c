5f449c5be2f4c8a6e724d52de5975fec1f4d58c7
Isaku Yamahata <yamahata@valinux.co.jp>
2012-07-04 16:03:08 +0900

    set-field: set-field: fixes
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- lib/set-field.c
+++ lib/set-field.c
@@ -113,7 +113,7 @@ set_field_check(const struct ofpact_reg_load *load, const struct flow *flow)
         return OFPERR_OFPBAC_BAD_ARGUMENT;
     }
     if (!mf_is_value_valid(mf, &load->value) ||
-        !mf_are_prereqs_ok(mf, flow)) {
+        (flow && !mf_are_prereqs_ok(mf, flow))) {
         return OFPERR_OFPBAC_BAD_ARGUMENT;
     }
     return 0;
@@ -123,6 +123,7 @@ enum ofperr
 set_field_from_openflow(
     const struct ofp12_action_set_field * oasf, struct ofpbuf *ofpacts)
 {
+    uint16_t len = ntohs(oasf->len);
     ovs_be32 *p = (ovs_be32*)oasf->field;
     uint32_t oxm_header = ntohl(*p);
     uint8_t oxm_length = NXM_LENGTH(oxm_header);
@@ -131,10 +132,10 @@ set_field_from_openflow(
     int i;
 
     /* ofp12_action_set_field is padded to 64 bits by zero */
-    if (oasf->len != ROUND_UP(sizeof(*oasf) + oxm_length, 8)) {
+    if (len != ROUND_UP(sizeof(*oasf) + oxm_length, 8)) {
         return OFPERR_OFPBAC_BAD_ARGUMENT;
     }
-    for (i = sizeof(*oasf) + oxm_length; i < oasf->len; i++) {
+    for (i = sizeof(*oasf) + oxm_length; i < len; i++) {
         if (((const char*)oasf)[i]) {
             return OFPERR_OFPBAC_BAD_ARGUMENT;
         }
@@ -161,12 +162,21 @@ set_field_to_openflow(const struct ofpact_reg_load *load,
     const struct mf_field *mf = load->dst.field;
     struct ofp12_action_set_field *oasf;
     ovs_be32* oxm_header;
-
+    uint16_t len;
+    uint16_t roundup;
     assert(load->ofpact.compat == OFPUTIL_OFPAT12_SET_FIELD);
+
+    len = sizeof(*oasf) + mf->n_bytes;
+    /* ofp12_action_set_field is padded to align 8 bytes */
+    roundup = ROUND_UP(len, 8);
+
     oasf = ofputil_put_OFPAT12_SET_FIELD(openflow);
     oxm_header = (ovs_be32*)oasf->field;
-    *oxm_header = mf->oxm_header;
-    memcpy(oasf + 1, &load->value, mf->n_bytes);
+    *oxm_header = htonl(mf->oxm_header);
+    oasf->len = htons(roundup);
+
+    ofpbuf_put(openflow, &load->value, mf->n_bytes);
+    ofpbuf_put_zeros(openflow, roundup - len);
 }
 
 void
